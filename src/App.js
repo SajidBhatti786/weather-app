833b377e547472267fb07cf648b628e6cc94d7af
sajid ali <113120555+SajidBhatti786@users.noreply.github.com>
2023-08-16 18:27:58 +0500

    exception handling and responsiveness fix

--- src/App.js
+++ src/App.js
@@ -1,59 +1,52 @@
-import React, { useEffect,useState } from 'react'
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  const[data, setData] = useState({})
+  const [data, setData] = useState({});
   const [location, setLocation] = useState('Ghotki');
   const [loading, setLoading] = useState(false);
-  const [error,setError] = useState(null)
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=58552952a15aed95c66e61e7b20710a3`
-  const searchLocation = (event) => {
-    if (event.key == 'Enter') {
+  const [error, setError] = useState(null);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=58552952a15aed95c66e61e7b20710a3`;
+
+  const searchLocation = async (event) => {
+    if (event.key === 'Enter') {
       setLoading(true);
-      axios.get(url)
-        .then((response) => {
-          if (response.status == 200) {
-            setError(null)
-            setData(response.data);
-            console.log(response);
-          }
-          
-          
-        })
-        .catch((error) => { 
-          if (error.response.status == 404)
-          {
-            console.log(error)
-          alert("no city found with this name!!")
-            
-          }
-          else {
-            alert("No internet access!")
-          }
-          
-        })
+      setError(null);
+
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          setError('City not found');
+        } else {
+          setError('No internet access');
+        }
+      } finally {
+        setLoading(false); // Ensure this line runs regardless of success or error
+      }
     }
-    setLoading(false);
-  }
+  };
+
   return (
-    
-    <div className='app'>
-       <div className={`overlay ${loading ? 'active' : ''}`}>
+    <div className="app">
+      <div className={`overlay ${loading ? 'active' : ''}`}>
         <h1>Loading...</h1>
       </div>
-      <div className='search'>
+      <div className="search">
         <input
-          type='text'
-          placeholder='Enter location'
+          type="text"
+          placeholder="Enter location"
           onKeyPress={searchLocation}
           onChange={(event) => setLocation(event.target.value)}
-
         />
       </div>
+
       <div className="container">
         {error && <p className="error">{error}</p>}
-      <div className="top">
+        {/* Rest of your components */}
+        <div className="top">
         <div className="location">
             <p>{data.name ? data.name: null}</p>
         </div>
@@ -76,12 +69,13 @@ function App() {
             
         </div>
         <div className='wind'>
-            <p className='bold'> { data.wind? data.wind.speed: null}Km/h</p>
+            <p className='bold'> { data.wind? data.wind.speed: null}MPH</p>
              <p>Wind Speed</p>
         </div>
       </div>
+        
       </div>
-      </div>
+    </div>
   );
 }
 
